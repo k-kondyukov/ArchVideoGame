93911f92fbee02cf703554919f4612c116637584
Сивиринов Иван <i.sivirinov@corp.mail.ru>
2025-06-30 18:11:44 +0300

    HW01
    - fix nullable and template constructor

--- Assets/Scripts/HW01/Singleton.cs
+++ Assets/Scripts/HW01/Singleton.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-abstract class Singleton<T> {
+abstract class Singleton<T> where T : class, new() {
+
     private static T _instance = null;
 
     public static T Instance() => _instance ??= new T();
